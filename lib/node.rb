84044856779b83e2b5f1d389907ab93a4a619014
Won Rhim <wwrk22@gmail.com>
2023-05-12 16:25:41 -0400

    Add LinkedList

--- lib/node.rb
+++ lib/node.rb
@@ -1,9 +1,9 @@
 class Node
   attr_accessor :data
-  attr_accessor :next
+  attr_accessor :next_node
 
-  def initialize(data, next)
+  def initialize(data=nil, next_node=nil)
     @data = data
-    @next = next
+    @next_node = next_node
   end
 end
